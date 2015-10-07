b208ef60bcc472d422b39b4e5304af7990ba4e1b
Pablo Varela <pablovarela182@gmail.com>
2015-10-08 00:21:54 +0200

    solves working on desktop app

--- js/main.js
+++ js/main.js
@@ -14,6 +14,3 @@ function init() {
   	Input();
 }
 
-if (typeof(require) == "function") {
-	window.$ = window.jQuery = require('./js/jquery.js') // This line is for a working ELectron app with jQuery )
-}
