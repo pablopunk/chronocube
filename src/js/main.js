265f8ffd5ba7b0cd2c2ad4808fc610d790643765
Pablo Varela <pablovarela182@gmail.com>
2016-03-02 20:54:06 +0100

    fixes electron app not opening

--- src/js/main.js
+++ src/js/main.js
@@ -2,7 +2,12 @@ var app = require('app');  // Module to control application life.
 var BrowserWindow = require('browser-window');  // Module to create native browser window.
 
 // Report crashes to our server.
-require('crash-reporter').start();
+require('crash-reporter').start({
+  productName: 'Chronocube',
+  companyName: 'Pablo Varela',
+  submitURL: 'https://github.com/pablopunk/chronocube/issues',
+  autoSubmit: true
+});
 
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the javascript object is GCed.
